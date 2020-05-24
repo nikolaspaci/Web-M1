4e662ff405325e318012644791b753a8ae9321d0
13tomoore <tom.moore@outlook.fr>
2020-05-24 16:27:01 +0200

    Ajout de la page récapitulative de la candidature

--- Candidater.php
+++ Candidater.php
@@ -12,33 +12,33 @@
    <body>
       <?php include ("modele/navbar.php") ?>
       <div class="content">
-         <form onsubmit="return controleFormulaire()">
+         <form action="Recapitulatif.php" method="post" onsubmit="return controleFormulaire()">
             <div class="form-row">
                <div class="form-group col">
                   <label for="InputNom">Nom</label>
-                  <input type="text" class="form-control" id="InputNom"  placeholder="Saisir votre nom">
+                  <input type="text" class="form-control" name="InputNom" id="InputNom"  placeholder="Saisir votre nom">
                </div>               
                <div class="form-group col">
                   <label for="InputPrenom">Prenom</label>
-                  <input type="text" class="form-control" id="InputPrenom" placeholder="Saisir votre prénom">
+                  <input type="text" class="form-control" name="InputPrenom" id="InputPrenom" placeholder="Saisir votre prénom">
                </div>
             </div>
             <div class="form-row">
                <div class="form-group col-8">
                   <label for="InputAdresse">Adresse</label>
-                  <input type="text" class="form-control" id="InputAdresse" placeholder="Saisir votre adresse">
+                  <input type="text" class="form-control" name="InputAdresse" id="InputAdresse" placeholder="Saisir votre adresse">
                </div>
             </div>
             <div class="form-row">
                <div class="form-group col-6">
                   <label for="DateNaissanceInput">Date de naissance</label>
-                  <input class="form-control" type="date" id="DateNaissanceInput">
+                  <input class="form-control" type="date" name="DateNaissanceInput" id="DateNaissanceInput">
                </div>
             </div>
             <div class="form-row">
                <div class="form-group col-6">
                   <label for="SelectParcours">Sélectionner votre parcours</label>
-                  <select single class="form-control" id="SelectParcours">
+                  <select single class="form-control" name="SelectParcours" id="SelectParcours">
                      <option>Licence</option>
                      <option>DUT</option>
                      <option>BTS</option>
@@ -48,19 +48,19 @@
             <div class="form-row">
                <div class="form-group col-6 col-md-3">
                   <label for="NoteMathsInput">Note en Mathématiques</label>
-                  <input class="form-control" type="number" id="NoteMathsInput">
+                  <input class="form-control" type="number" name="NoteMathsInput" id="NoteMathsInput">
                </div>
                <div class="form-group col-6 col-md-3">
                   <label for="NoteInfoInput">Note en Informatique</label>
-                  <input class="form-control" type="number" id="NoteInfoInput">
+                  <input class="form-control" type="number" name="NoteInfoInput" id="NoteInfoInput">
                </div>
                <div class="form-group col-6 col-md-3">
                   <label for="NoteAnglaisInput">Note en Anglais</label>
-                  <input class="form-control" type="number" id="NoteAnglaisInput">
+                  <input class="form-control" type="number" name="NoteAnglaisInput" id="NoteAnglaisInput">
                </div>
                <div class="form-group col-6 col-md-3">
                   <label for="NoteMoyenneInput">Moyenne générale</label>
-                  <input class="form-control" type="number" id="NoteMoyenneInput">
+                  <input class="form-control" type="number" name="NoteMoyenneInput" id="NoteMoyenneInput">
                </div>
             </div>
             <div class="form-row">
