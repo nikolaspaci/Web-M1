86a688532af874d8b0ca3a91a56e9fc9c50b5f35
13tomoore <tom.moore@outlook.fr>
2020-06-01 11:05:50 +0200

    Comparatif des deux candidatures

---
+++ CandidatureExistante.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<html lang="fr">
+   <head>
+   
+   </head>
+   <body>
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h3>Candidature actuelle</h3>
+                        <div class="form-row">
+                            <div class="form-group col">
+                                <p>Nom</p>
+                                <p class="font-weight-bold"><?php echo $etudiantExistant['Nom'] ?></p>
+                            </div>               
+                            <div class="form-group col">
+                                <p>Prénom</p>
+                                <p class="font-weight-bold"><?php echo $etudiantExistant['Prenom'] ?></p>                
+                            </div>            
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col">
+                                <p>Adresse</p>
+                                <p class="font-weight-bold"><?php echo $etudiantExistant['AdresseEmail'] ?></p>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col">
+                                <p>Date de naissance</p>
+                                <p class="font-weight-bold"><?php echo date("d/m/Y",strtotime($etudiantExistant['DateNaissance'])) ?></p>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col">
+                                <p>Parcours</p>
+                                <p class="font-weight-bold"><?php echo $etudiantExistant['Parcours'] ?></p>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col">
+                                <p>Note en Mathématiques</p>
+                                <p class="font-weight-bold"><?php echo $etudiantExistant['NoteMath'] ?></p>
+                            </div>
+                            <div class="form-group col">
+                                <p>Note en Informatique</p>
+                                <p class="font-weight-bold"><?php echo $etudiantExistant['NoteInformatique'] ?></p>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col">
+                                <p>Note en Anglais</p>
+                                <p class="font-weight-bold"><?php echo $etudiantExistant['NoteAnglais'] ?></p>
+                            </div>
+                            <div class="form-group col">
+                                <p>Moyenne générale</p>
+                                <p class="font-weight-bold"><?php echo $etudiantExistant['Moyenne'] ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <h3>Nouvelle candidature</h3>
+                        <div class="form-row">
+                            <div class="form-group col">
+                                <p>Nom</p>
+                                <p class="font-weight-bold"><?php echo $candidat['InputNom'] ?></p>
+                            </div>               
+                            <div class="form-group col">
+                                <p>Prénom</p>
+                                <p class="font-weight-bold"><?php echo $candidat['InputPrenom'] ?></p>                
+                            </div>            
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col">
+                                <p>Adresse</p>
+                                <p class="font-weight-bold"><?php echo $candidat['InputAdresse'] ?></p>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col">
+                                <p>Date de naissance</p>
+                                <p class="font-weight-bold"><?php echo date("d/m/Y",strtotime($candidat['DateNaissanceInput'])) ?></p>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col">
+                                <p>Parcours</p>
+                                <p class="font-weight-bold"><?php echo $candidat['SelectParcours'] ?></p>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col">
+                                <p>Note en Mathématiques</p>
+                                <p class="font-weight-bold"><?php echo $candidat['NoteMathsInput'] ?></p>
+                            </div>
+                            <div class="form-group col">
+                                <p>Note en Informatique</p>
+                                <p class="font-weight-bold"><?php echo $candidat['NoteInfoInput'] ?></p>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col">
+                                <p>Note en Anglais</p>
+                                <p class="font-weight-bold"><?php echo $candidat['NoteAnglaisInput'] ?></p>
+                            </div>
+                            <div class="form-group col">
+                                <p>Moyenne générale</p>
+                                <p class="font-weight-bold"><?php echo $candidat['NoteMoyenneInput'] ?></p>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div class='float-right'>
+                <button type='button' class='btn btn-secondary pull-right' data-toggle='modal' data-target='#annulerModal'>Annuler</button>
+                <button type='button' class='btn btn-danger pull-right' data-toggle='modal' data-target='#supprimerModal'>Supprimer</button>
+                <button type='button' class='btn btn-primary pull-right' data-toggle='modal' data-target='#sauvegarderModal'>Sauvegarder</button>
+            </div>
+            <div class="modal fade" id="annulerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header"> 
+                            <h5 class="modal-title">Etes-vous sûr de vouloir annuler ?</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times</span>
+                            </button>
+                        </div>
+                        <div class="modal-footer">
+                            <form action="Accueil.php" metho="POST">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Non</button>
+                                <button type="submit" class="btn btn-primary">Oui</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal fade" id="supprimerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header"> 
+                            <h5 class="modal-title">Etes-vous sûr de vouloir supprimer ?</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times</span>
+                            </button>
+                        </div>
+                        <div class="modal-footer">
+                            <form action="SuppressionCandidature.php" metho="POST">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Non</button>
+                                <button type="submit" class="btn btn-primary">Oui</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal fade" id="sauvegarderModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header"> 
+                            <h5 class="modal-title">Etes-vous sûr de vouloir sauvegarder ?</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times</span>
+                            </button>
+                        </div>
+                        <div class="modal-footer">
+                            <form action="ChangementCandidature.php" metho="POST">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Non</button>
+                                <button type="submit" class="btn btn-primary">Oui</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+   </body>
+</html>
--- ChangementCandidature.php
+++ ChangementCandidature.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
-    $candidat=array();
-    $candidat = $_SESSION['GestionCandidat'];
-    unset ( $_SESSION['GestionCandidat'] );
+    $etudiantExistant=array();
+    $etudiantExistant = $_SESSION['CandidatureExistante'];
+    unset ( $_SESSION['CandidatureExistante'] );
 
     $serveur="localhost";
     $login="root";
@@ -11,10 +11,10 @@
     $connexion=new PDO("mysql:host=$serveur;dbname=MasterApp;",$login,$pass);
     $connexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     $sqlUpdate="UPDATE etudiant
-                SET Nom='" .$candidat['InputNom'] ."',Prenom='" .$candidat['InputPrenom'] ."',AdresseEmail='" .$candidat['InputAdresse'] ."',DateNaissance='" .$candidat['DateNaissanceInput'] ."',Parcours='" .$candidat['SelectParcours'] ."',NoteMath=" .$candidat['NoteMathsInput'] .",NoteInformatique=" .$candidat['NoteInfoInput'] .",NoteAnglais=" .$candidat['NoteAnglaisInput'] .",Moyenne=" .$candidat['NoteMoyenneInput']
-                ." WHERE Prenom='" .$candidat['InputPrenom'] ."'
-                AND Nom='" .$candidat['InputNom'] ."'
-                AND DateNaissance ='" .$candidat['DateNaissanceInput'] ."'";
+                SET Nom='" .$etudiantExistant['InputNom'] ."',Prenom='" .$etudiantExistant['InputPrenom'] ."',AdresseEmail='" .$etudiantExistant['InputAdresse'] ."',DateNaissance='" .$etudiantExistant['DateNaissanceInput'] ."',Parcours='" .$etudiantExistant['SelectParcours'] ."',NoteMath=" .$etudiantExistant['NoteMathsInput'] .",NoteInformatique=" .$etudiantExistant['NoteInfoInput'] .",NoteAnglais=" .$etudiantExistant['NoteAnglaisInput'] .",Moyenne=" .$etudiantExistant['NoteMoyenneInput']
+                ." WHERE Prenom='" .$etudiantExistant['InputPrenom'] ."'
+                AND Nom='" .$etudiantExistant['InputNom'] ."'
+                AND DateNaissance ='" .$etudiantExistant['DateNaissanceInput'] ."'";
                 
     $connexion->exec($sqlUpdate);
 
